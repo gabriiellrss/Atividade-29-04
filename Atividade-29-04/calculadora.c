8fb4373ada74cc521c64db9fe0a129c2beeaf101
gabriiellrss <gabriel.santana166@etec.sp.gov.br>
2024-05-06 16:31:34 -0300

    teste

--- Atividade-29-04/calculadora.c
+++ Atividade-29-04/calculadora.c
@@ -1,14 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+
+
 void main  () {
 	
+	
 	int op, n1 ,n2 ,r;
+	op =1;
 
-	while (op>=0){
+	while (op>0){
 
 	printf("\nEscolha uma das seguintes opcoes:\n");
 	printf("1)Soma\n");
 	printf("2)Subtracao\n");
 	printf("3)Multiplicacao\n");
 	printf("4)Divisao\n");
+	printf("5)Sair\n");
 	scanf("%d",&op);
 
 	
@@ -19,7 +28,7 @@ void main  () {
 			scanf("%d",&n1);
 			printf("Digite o segundo numero:\n");
 			scanf("%d",&n2);
-			printf("A soma dos numeros:%d e %d resulta em: %d",n1 ,n2 , r=n1+2);
+			printf("A soma dos numeros:%d e %d resulta em: %d",n1 ,n2 , r=n1+n2);
 		break;
 		case 2 :
 			printf("Subtracao\n");
@@ -62,6 +71,10 @@ void main  () {
                 printf("A divisao dos numeros:%d e %d resulta em: %d \n",n1 ,n2, r=n1/n2);
                 break;
             }
+
+		case 5:
+	    op = 0;
+		  break;
         }
     }
  }
